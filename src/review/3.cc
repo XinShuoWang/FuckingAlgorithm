38c0bcd476bd20544ea05563e08417b6c62bffc0
Your Name <you@example.com>
2021-08-23 11:02:09 +0800

    2021-8-23 11:02

--- src/review/3.cc
+++ src/review/3.cc
@@ -29,3 +29,29 @@ TEST(leetcode_3, 1) {
     }
   };
 }
+
+TEST(leetcode_3, 2) {
+  using namespace std;
+  class Solution {
+   public:
+    int lengthOfLongestSubstring(string s) {
+      if (s.length() == 0) return 0;
+      set<char> ss;
+      int j = 0, ans = 0;
+      for (int i = 0; i < s.length(); ++i) {
+        if (i != 0) {
+          ss.erase(s[i - 1]);
+        }
+        while (j < s.length() && ss.count(s[j]) == 0) {
+          ss.insert(s[j]);
+          j++;
+        }
+        ans = max(ans, j - i);
+      }
+      return ans;
+    }
+  };
+
+  Solution solution;
+  cout << solution.lengthOfLongestSubstring("bbbbb") << endl;
+}
